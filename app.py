496e4f42ad67edf27021f966f787c21cd8c09e3a
gautam <jarvis.gomzi@gmail.com>
2020-09-23 21:16:35 +0530

    modified

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from flask import Flask
 from flask_restful import Api 
 from flask_jwt import JWT
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister,User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['PROPAGATE_EXCEPTION'] = True
 app.secret_key='1996'
 api = Api(app)
 
@@ -27,9 +28,10 @@ api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
+api.add_resource(User,'/user/<int:user_id>')
 
 if __name__ == '__main__':
-    
+    from db import db
     db.init_app(app)
     app.run(port=5002,debug=True)
 
--- models/user.py
+++ models/user.py
@@ -10,46 +10,30 @@ class UserModel(db.Model):
     password = db.Column(db.String(80))
 
     def __init__(self,username,password):
-        
         self.username =username
         self.password =password
 
+    def json(self):
+        return {
+            'id':self.id,
+            'username':self.username
+
+        }
+
     def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
     def find_by_username(cls,username):
         return cls.query.filter_by(username=username).first()
-        ''''connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query= "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query,(username,))
-        row= result.fetchone()
-        if row:
-            user=cls(*row)
-
-        else:
-            user=None
-
-        connection.close()
-        return user'''
+        
 
     @classmethod
     def find_by_id(cls,_id):
         return cls.query.filter_by(id=_id).first()
-        '''connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query= "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query,(id,))
-        row= result.fetchone()
-        if row:
-            user=cls(*row)
-
-        else:
-            user=None
-
-        connection.close()
-        return user'''
+        
