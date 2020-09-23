5cd47d8d2f1ead0979f1d693901329677eb8032f
gautam <jarvis.gomzi@gmail.com>
2020-09-23 21:30:40 +0530

    modified

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from security import authenticate, identity
 from resources.user import UserRegister,User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
 
 
 
@@ -31,7 +32,7 @@ api.add_resource(UserRegister,'/register')
 api.add_resource(User,'/user/<int:user_id>')
 
 if __name__ == '__main__':
-    from db import db
+    
     db.init_app(app)
     app.run(port=5002,debug=True)
 
